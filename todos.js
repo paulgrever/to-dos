2b3e721eeaa0c8f7495e9a5ea34cdfb0d9a3c0e3
Paul Grever <paulgrever@gmail.com>
2015-08-18 23:28:12 -0600

    finished chapter 5 routing part 2

--- todos.js
+++ todos.js
@@ -1,7 +1,16 @@
 Todos = new Meteor.Collection('todos');
+Lists = new Meteor.Collection('lists');
 
 Router.route('/register');
 Router.route('/login');
+Router.route('/list/:_id', {
+  name: 'listPage',
+  template : 'listPage',
+  data: function(){
+    var currentList = this.params._id;
+    return Lists.findOne({ _id: currentList });
+  }
+});
 Router.route('/', {
   name : 'home',
   template: 'home'
@@ -14,7 +23,8 @@ if(Meteor.isClient){
     // client code goes here
   Template.todos.helpers({
     'todo' : function(){
-      return Todos.find({}, {sort: {createdAt: -1}}); 
+      var currentList = this._id;
+      return Todos.find({ listId: currentList }, {sort: {createdAt: -1}})
     }
   });
 
@@ -31,21 +41,46 @@ if(Meteor.isClient){
 
   Template.todosCount.helpers({
     'totalTodos' : function(){
-      return Todos.find().count();
+      var currentList = this._id;
+      return Todos.find({listId: currentList}).count();
     },
     'completedTodos' : function(){
-      return Todos.find({completed: true}).count();
+      var currentList = this._id;
+      return Todos.find({completed: true, listId : currentList}).count();
     }
   });
+
+  Template.lists.helpers({
+    'list' : function(){
+      return Lists.find({}, {sort: {name: 1}});
+    }
+  });
+
+  Template.addList.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var listName = $('[name=listName]').val();
+      Lists.insert({
+          name: listName
+      }, function(error, results){
+        Router.go('listPage', {_id: results});
+
+      });
+      $('[name=listName]').val('');
+    }
+});
 
   Template.addTodo.events({
     'submit form' : function(event){
       event.preventDefault();
       var todoName = $('[name="todoName"]').val();
+      var currentList = this._id;
       Todos.insert({
         name : todoName,
         completed : false, 
-        createdAt : new Date()
+        createdAt : new Date(),
+        listId : currentList
+
       });
       $('[name="todoName').val('');
     }
