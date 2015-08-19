2ffeca319b4efafc16a455e5a19c5b3f527eb94f
Paul Grever <paulgrever@gmail.com>
2015-08-18 20:51:00 -0600

    finished chapter 2

--- todos.js
+++ todos.js
@@ -1,23 +1,38 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Todos = new Meteor.Collection('todos');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+if(Meteor.isClient){
+    // client code goes here
+  Template.todos.helpers({
+    'todo' : function(){
+      return Todos.find({}, {sort: {createdAt: -1}}); 
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.addTodo.events({
+    'submit form' : function(event){
+      event.preventDefault();
+      var todoName = $('[name="todoName"]').val();
+      Todos.insert({
+        name : todoName,
+        completed : false, 
+        createdAt : new Date()
+      });
+      $('[name="todoName').val('');
+    }
+  });
+
+  Template.todoItem.events({
+    'click .delete-todo' : function(event){
+      event.preventDefault();
+      var documentId = this._id;
+      var confirm = window.confirm("Delete this task?")
+      if (confirm) {
+        Todos.remove({_id: documentId});
+      }    
     }
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+if(Meteor.isServer){
+    // server code goes here
 }
