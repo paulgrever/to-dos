293f9f2660631751847601d32ac198ac275f6962
Paul Grever <paulgrever@gmail.com>
2015-08-20 16:29:51 -0600

    finished chapter 7

--- todos.js
+++ todos.js
@@ -4,12 +4,35 @@ Lists = new Meteor.Collection('lists');
 Router.route('/register');
 Router.route('/login');
 Router.route('/list/:_id', {
-  name: 'listPage',
-  template : 'listPage',
-  data: function(){
-    var currentList = this.params._id;
-    return Lists.findOne({ _id: currentList });
-  }
+    name: 'listPage',
+    template: 'listPage',
+    data: function(){
+        var currentList = this.params._id;
+        var currentUser = Meteor.userId();
+        return Lists.findOne({ _id: currentList, createdBy: currentUser });
+    },
+    onRun: function(){
+        console.log("You triggered 'onRun' for 'listPage' route.");
+        this.next();
+    },
+    onRerun: function(){
+        console.log("You triggered 'onRerun' for 'listPage' route.");
+    },
+    onBeforeAction: function(){
+        console.log("You triggered 'onBeforeAction' for 'listPage' route.");
+        var currentUser = Meteor.userId();
+        if(currentUser){
+            this.next();
+        } else {
+            this.render("login");
+        }
+    },
+    onAfterAction: function(){
+        console.log("You triggered 'onAfterAction' for 'listPage' route.");
+    },
+    onStop: function(){
+        console.log("You triggered 'onStop' for 'listPage' route.");
+    }
 });
 Router.route('/', {
   name : 'home',
@@ -100,7 +123,10 @@ if(Meteor.isClient){
         if(error){
           console.log(error.reason);
         } else {
-          Router.go("home")
+          var currentRoute = Router.current().route.getName();
+          if (currentRoute == "login") {
+           Router.go("home");
+         } 
         }
       });
       $('[name=email]').val("");
